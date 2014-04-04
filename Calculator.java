c6cf8e065a259a2ea03a505a02524c5228cd9e55
Wiebke Vogel <WiebkeVogel@wiebi.congstar.box>
2014-04-04 21:17:19 +0200

    added substraction

--- Calculator.java
+++ Calculator.java
@@ -5,6 +5,11 @@ public class Calculator{
 		System.out.println("Bitte eingeben [+-*/] zahl1 zahl2: ");
 		String operator = scanner.next();
 		int zahl1 = scanner.nextInt();
-		int zahl2 = scanner.nextInt();	
+		int zahl2 = scanner.nextInt();
+		int result = 0;	
+		if (operator.equals("-")){
+			result = zahl1 - zahl2;
+		}
+		System.out.println(zahl1 + " " + operator + " " + zahl2 + " = " + result);
 	}
 }
